148bef988325f49f443051433092679dca1b39d8
박다정 <daaa08@naver.com>
2017-07-04 10:56:59 +0900

    firebase storage upload

--- app/src/main/java/com/example/da08/firebasebbs/DetailActivity.java
+++ app/src/main/java/com/example/da08/firebasebbs/DetailActivity.java
@@ -1,15 +1,24 @@
 package com.example.da08.firebasebbs;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.da08.firebasebbs.domain.Bbs;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.util.Date;
 
 public class DetailActivity extends AppCompatActivity {
@@ -19,20 +28,52 @@ public class DetailActivity extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference bbsRef;
+    // storage
+    private StorageReference mStorageRef;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        // database 레퍼런스
         database = FirebaseDatabase.getInstance();
         bbsRef = database.getReference("bbs");
+        // storage 레퍼런스
+        mStorageRef = FirebaseStorage.getInstance().getReference("images");  // getReference를 기준으로 객체들이 들어감, mStorageRef도 동일
 
         editTitle = (EditText)findViewById(R.id.editTitle);
         editAuthor = (EditText)findViewById(R.id.editAuthor);
         editContent = (EditText)findViewById(R.id.editContent);
         btnSave  = (Button)findViewById(R.id.btnSave);
+    }
+
+
+    // storage _upload a file
+    public void upLoadFile(String filePath){
+        File file = new File(filePath);  // 스마트폰에있는 파일 업로드 경로
+        Uri uri = Uri.fromFile(file);
+        // 파이어 베이스의 파일 업로드 경로
+        String fileName = file.getName();
+        StorageReference riversRef = mStorageRef.child(fileName);
+
+        riversRef.putFile(uri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+//                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Log.e("storage","success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        Log.e("storage","fail"+exception.getMessage());
+                    }
+                });
     }
 
     // 데이터 전송
